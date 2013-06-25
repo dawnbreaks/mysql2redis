27da07ccb43f0772fc1f4a6a470deeedf29f1136
lubinwang <308181687@qq.com>
2013-06-25 13:23:37 +0800

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,20 @@
+#include "utils.h"
+
+/**
+ * check the parameter *host valid or not.
+ *
+ * Maybe, it is no need to do so
+ */
+unsigned int check_host(const char* host)
+{
+  return 1;
+}
+
+/**
+ * see check_host
+ */
+unsigned int check_ip(const char* ip)
+{
+	return 1;
+}
+
